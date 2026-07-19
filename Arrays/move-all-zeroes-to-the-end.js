0a4c9a3c40d52ab9ec4669a5d98980e19908b84a
Shweta Thakur <2shwetathakur@gmail.com>
2026-07-19 23:44:47 +0530

    (array): solve move all zeroes to the end using two pointers

---
+++ Arrays/move-all-zeroes-to-the-end.js
@@ -0,0 +1,16 @@
+function moveZeroes(arr) {
+    let j = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== 0) {
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+            j++;
+        }
+    }
+
+    return arr;
+}
+
+const arr = [0, 1, 0, 3, 12];
+
+console.log(moveZeroes(arr));
