1c677be2105d4bb3970252b4b7bb2633f4851ed3
Shweta Thakur <2shwetathakur@gmail.com>
2026-08-08 23:41:42 +0530

    feat: implement find the difference

---
+++ HashMap&Sets/find-the-difference.js
@@ -0,0 +1,15 @@
+function findTheDifference(s, t) {
+  let result = 0;
+
+  for (const char of s) {
+    result ^= char.charCodeAt(0);
+  }
+
+  for (const char of t) {
+    result ^= char.charCodeAt(0);
+  }
+
+  return String.fromCharCode(result);
+}
+
+module.exports = findTheDifference;
