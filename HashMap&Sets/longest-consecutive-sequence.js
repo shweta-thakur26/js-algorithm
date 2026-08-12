9f27da3c3a99af9c04fc9d746a3a36398819cf86
Shweta Thakur <2shwetathakur@gmail.com>
2026-08-12 19:59:59 +0530

    feat: solve longest consecutive sequence

---
+++ HashMap&Sets/longest-consecutive-sequence.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function longestConsecutive(nums) {
+  const numSet = new Set(nums);
+  let longest = 0;
+
+  for (const num of numSet) {
+    // Start counting only if this is the beginning of a sequence.
+    if (!numSet.has(num - 1)) {
+      let current = num;
+      let length = 1;
+
+      while (numSet.has(current + 1)) {
+        current++;
+        length++;
+      }
+
+      longest = Math.max(longest, length);
+    }
+  }
+
+  return longest;
+}
+
+module.exports = longestConsecutive;
