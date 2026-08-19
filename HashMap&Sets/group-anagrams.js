11ed4d36a3137d31b7f794957895877e8d152c45
Shweta Thakur <2shwetathakur@gmail.com>
2026-08-19 22:01:49 +0530

    Variables name changing

--- HashMap&Sets/group-anagrams.js
+++ HashMap&Sets/group-anagrams.js
@@ -4,13 +4,13 @@ function groupAnagrams(strs) {
   const groups = new Map();
 
   for (const str of strs) {
-    const key = str.split("").sort().join("");
+    const keys = str.split("").sort().join("");
 
-    if (!groups.has(key)) {
-      groups.set(key, []);
+    if (!groups.has(keys)) {
+      groups.set(keys, []);
     }
 
-    groups.get(key).push(str);
+    groups.get(keys).push(str);
   }
 
   return Array.from(groups.values());
