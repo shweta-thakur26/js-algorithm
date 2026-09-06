c3609321496a5cc60ff3468cd0faae91fd5511dc
Shweta Thakur <2shwetathakur@gmail.com>
2026-09-06 22:25:45 +0530

    Intersection Changes

--- HashMap&Sets/intersection-of-two-arrays.js
+++ HashMap&Sets/intersection-of-two-arrays.js
@@ -17,5 +17,5 @@ function intersection(nums1, nums2) {
 }
 
 // Example usage
-console.log(intersection([1, 2, 2, 1], [2, 2]));       // [2]
+console.log(intersection([1, 2, 4, 2, 1], [2, 2]));       // [2]
 console.log(intersection([4, 9, 5], [9, 4, 9, 8, 4])); // [9, 4]
