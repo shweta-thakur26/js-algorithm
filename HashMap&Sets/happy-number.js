5d4d1cb7f049f34e2343d68f57a0973f1b8df601
Shweta Thakur <2shwetathakur@gmail.com>
2026-09-15 22:36:52 +0530

    Changes

--- HashMap&Sets/happy-number.js
+++ HashMap&Sets/happy-number.js
@@ -1,12 +1,12 @@
 // feat: implement happy number checker
 
-function isHappyNumber(numbers) {
+function isHappyNumber(num) {
   const seen = new Set();
 
-  while (numbers !== 1 && !seen.has(numbers)) {
-    seen.add(numbers);
+  while (num !== 1 && !seen.has(num)) {
+    seen.add(num);
 
-    numbers = String(numbers)
+    numbers = String(num)
       .split("")
       .reduce((sum, digit) => sum + Number(digit) ** 2, 0);
   }
