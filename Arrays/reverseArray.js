a4aa4cb2995cedee4d446f4426302d5f521c8201
Shweta Thakur <2shwetathakur@gmail.com>
2026-09-23 23:29:25 +0530

    Changes

--- Arrays/reverseArray.js
+++ Arrays/reverseArray.js
@@ -8,7 +8,7 @@
 function reverseArray(arr) {
     const result = [];
 
-    for (let j = arr.length - 1; j >= 0; j--) {
+    for (let i = arr.length - 1; i >= 0; i--) {
         result.push(arr[i]);
     }
 
